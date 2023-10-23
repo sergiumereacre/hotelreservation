46e67abd9dd63b88fd464324ef8929146f7edf4d
Edison <20241135@studentmail.ul.ie>
2023-10-23 23:08:28 +0100

    Starting on IHotelDetails interface

--- backend/src/main/java/com/hotel/reservations/interfaces/IHotelDetails.java
+++ backend/src/main/java/com/hotel/reservations/interfaces/IHotelDetails.java
@@ -1,5 +1,18 @@
 package com.hotel.reservations.interfaces;
 
+import java.util.Date;
+import java.util.List;
+
+import com.hotel.reservations.entity.Room;
+
 public interface IHotelDetails {
+
+    String getHotelDetails();
+
+    List<Room> getRooms();
+
+    String getRoomDetails(int roomId);
+
+    boolean getRoomIsAvailable(int roomId, Date startDate, Date endDate);
     
 }
