ad1aeccd637e90b860cd5f9222e294eed6a64627
David Walsh <20276885@studentmail.ul.ie>
2023-11-01 23:26:46 +0000

    Update DiscountController

--- backend/src/main/java/com/hotel/discount/controller/DiscountController.java
+++ backend/src/main/java/com/hotel/discount/controller/DiscountController.java
@@ -1,5 +1,10 @@
 package com.hotel.discount.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/discount")
 public class DiscountController {
     
 }
