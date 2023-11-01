f4656f5086540a6d96ca3d0f1f150309dc5aa0cc
David Walsh <20276885@studentmail.ul.ie>
2023-11-01 23:10:14 +0000

    Quick changes need updating

--- backend/src/main/java/com/hotel/discount/interfaces/IDiscountMgt.java
+++ backend/src/main/java/com/hotel/discount/interfaces/IDiscountMgt.java
@@ -1,7 +1,9 @@
 package com.hotel.discount.interfaces;
 
 public interface IDiscountMgt {
-    void applySimpleDiscount(IChargeable chargeable, double flatDiscount, double percentageDiscount, int applierId);
+    // Waiting for IChargeable interface
+    //void applySimpleDiscount(IChargeable chargeable, double flatDiscount, double percentageDiscount, int applierId);
 
-    void applyLoyaltyDiscount(IChargeable chargeable, ILoyaltyStatus loyaltyStatus);
+    // Waiting for IChargeable and ILoyaltyStatus interfaces
+    //void applyLoyaltyDiscount(IChargeable chargeable, ILoyaltyStatus loyaltyStatus);
 }
---
+++ backend/src/main/java/com/hotel/discount/service/SimpleDiscountService.java
@@ -0,0 +1,5 @@
+package com.hotel.discount.service;
+
+public class SimpleDiscountService extends DiscountDecorator {
+    
+}
