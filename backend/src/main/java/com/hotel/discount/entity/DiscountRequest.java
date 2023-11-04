057ea3c1d3dd4e3b41f2e22ae1cacaa71bb3b16b
David Walsh <20276885@studentmail.ul.ie>
2023-11-04 18:20:54 +0000

    Adding functionality to DiscountRequest

--- backend/src/main/java/com/hotel/discount/entity/DiscountRequest.java
+++ backend/src/main/java/com/hotel/discount/entity/DiscountRequest.java
@@ -3,11 +3,77 @@ package com.hotel.discount.entity;
 // DiscountRequest.java (Hypothetical DTO class)
 public class DiscountRequest {
 
-    private IChargeable chargeable;
     private double flatDiscount;
     private double percentageDiscount;
     private int applierId;
+    // Assuming that chargeable and loyaltyStatus are simple enough to be serialized directly;
+    // otherwise, you would use some form of identifier to look up the relevant objects.
+    private IChargeable chargeable;
     private ILoyaltyStatus loyaltyStatus;
 
-    // Getters and setters for each field would be here
+    // Default constructor for JSON deserialization
+    public DiscountRequest() {
+    }
+
+    // Full constructor for all fields
+    public DiscountRequest(IChargeable chargeable, double flatDiscount, double percentageDiscount, int applierId, ILoyaltyStatus loyaltyStatus) {
+        this.chargeable = chargeable;
+        this.flatDiscount = flatDiscount;
+        this.percentageDiscount = percentageDiscount;
+        this.applierId = applierId;
+        this.loyaltyStatus = loyaltyStatus;
+    }
+
+    // Getters and setters
+    public IChargeable getChargeable() {
+        return chargeable;
+    }
+
+    public void setChargeable(IChargeable chargeable) {
+        this.chargeable = chargeable;
+    }
+
+    public double getFlatDiscount() {
+        return flatDiscount;
+    }
+
+    public void setFlatDiscount(double flatDiscount) {
+        this.flatDiscount = flatDiscount;
+    }
+
+    public double getPercentageDiscount() {
+        return percentageDiscount;
+    }
+
+    public void setPercentageDiscount(double percentageDiscount) {
+        this.percentageDiscount = percentageDiscount;
+    }
+
+    public int getApplierId() {
+        return applierId;
+    }
+
+    public void setApplierId(int applierId) {
+        this.applierId = applierId;
+    }
+
+    public ILoyaltyStatus getLoyaltyStatus() {
+        return loyaltyStatus;
+    }
+
+    public void setLoyaltyStatus(ILoyaltyStatus loyaltyStatus) {
+        this.loyaltyStatus = loyaltyStatus;
+    }
+
+    // toString method for logging and debugging purposes
+    @Override
+    public String toString() {
+        return "DiscountRequest{" +
+                "flatDiscount=" + flatDiscount +
+                ", percentageDiscount=" + percentageDiscount +
+                ", applierId=" + applierId +
+                ", chargeable=" + chargeable +
+                ", loyaltyStatus=" + loyaltyStatus +
+                '}';
+    }
 }
