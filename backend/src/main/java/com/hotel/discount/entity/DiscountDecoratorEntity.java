9c833554b421b7e122762902ea985cdc5915f9dd
David Walsh <20276885@studentmail.ul.ie>
2023-11-04 17:58:02 +0000

    Small updates and fixes

--- backend/src/main/java/com/hotel/discount/entity/DiscountDecoratorEntity.java
+++ backend/src/main/java/com/hotel/discount/entity/DiscountDecoratorEntity.java
@@ -2,17 +2,27 @@ package com.hotel.discount.entity;
 
 import javax.persistence.Entity;
 
-// Decorator interface
+// Abstract decorator class
 @Entity
 public abstract class DiscountDecoratorEntity implements IChargeable {
     protected IChargeable chargeable;
     protected double flatDiscount;
     protected double percentageDiscount;
 
-    public DiscountDecoratorEntity(IChargeable chargeable) {
+    DiscountDecoratorEntity(IChargeable chargeable) {
         this.chargeable = chargeable;
     }
 
-    public abstract double getPrice();
-    public abstract String getDiscountDetails();
+    @Override
+    public double getPrice() {
+        return chargeable.getPrice();
+    }
+
+    @Override
+    public String getDiscountDetails() {
+        return chargeable.getDiscountDetails();
+    }
+
+    //public abstract double getPrice();
+    //public abstract String getDiscountDetails();
 }
