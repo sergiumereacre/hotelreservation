c09da0c9ec26a72c10391b0d30458fa2e5dd8ddd
Edison <20241135@studentmail.ul.ie>
2023-11-09 14:53:41 +0000

    make reservation request body

--- backend/src/main/java/com/hotel/reservations/controller/ReservationController.java
+++ backend/src/main/java/com/hotel/reservations/controller/ReservationController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.hotel.payments.entity.PaymentEntity;
 import com.hotel.reservations.entity.ReservationEntity;
 import com.hotel.reservations.service.ReservationService;
 import com.hotel.reservations.service.EngageReservationService;
