a4f219530c181e8105c2aec5fa19d4ca19927916
David Walsh <20276885@studentmail.ul.ie>
2023-11-12 19:08:25 +0000

    Tiny changes

--- backend/src/main/java/com/hotel/accounts/repository/AccountRepository.java
+++ backend/src/main/java/com/hotel/accounts/repository/AccountRepository.java
@@ -1,13 +1,14 @@
 package com.hotel.accounts.repository;
 
-import com.hotel.accounts.entity.AccountEntity;
-import com.hotel.accounts.entity.GuestAccountEntity;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.hotel.accounts.entity.AccountEntity;
+import com.hotel.accounts.entity.GuestAccountEntity;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
--- backend/src/main/java/com/hotel/discounts/service/DiscountMgt.java
+++ backend/src/main/java/com/hotel/discounts/service/DiscountMgt.java
@@ -4,12 +4,13 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.hotel.accounts.entity.AccountEntity;
 import com.hotel.accounts.repository.AccountRepository;
 import com.hotel.discounts.interfaces.IDiscountMgt;
 
-// Note: Creation of percentage discounts by staff (limited to a certain percentage off) for bookings. Only one discount can be applied per reservation. 
+// Note: Creation of percentage discounts by staff (limited to a certain percentage off) for bookings. Only one discount can be applied per reservation.
 
-// ConcreteComponent (Decorator Pattern)
+// ConcreteComponent (Decorator Pattern) RESERVATION SHOULD BE THE CONCRETE COMPONENT
 @Service
 class DiscountMgt implements IDiscountMgt {
     // Sort out account stuff here
@@ -22,7 +23,8 @@ class DiscountMgt implements IDiscountMgt {
     // Note update account stuff, currently wrong
     @Override
     public void applySimpleDiscount(IChargeable chargeable, double flatDiscount, double percentageDiscount, int applierId) {
-        Optional<Account> accountOptional = accountRepository.findById(applierId);
+
+        Optional<AccountEntity> accountOptional = accountRepository.findByIdAndIsStaff((long) applierId, isStaff); // Need to check staff
         accountOptional.ifPresent(account -> {
             if (percentageDiscount <= 75 && account.isStaff()) {
                 chargeable = new SimpleDiscount(chargeable, flatDiscount, percentageDiscount);
@@ -33,7 +35,7 @@ class DiscountMgt implements IDiscountMgt {
     // Need to update business logic
     @Override
     public void applyLoyaltyDiscount(IChargeable chargeable, ILoyaltyStatus loyaltyStatus) {
-        Optional<Account> accountOptional = accountRepository.findById(loyaltyStatus.getUserId());
+        Optional<AccountEntity> accountOptional = accountRepository.findById(loyaltyStatus.getUserId());
         accountOptional.ifPresent(account -> {
             if (account.isStaff()) {
                 chargeable = new LoyaltyDiscount(chargeable, loyaltyStatus);
