16af2a8ae4b2bc0b88841cca00948b1c9e107792
TheCaister <edisoncai2001@gmail.com>
2023-11-12 14:21:40 +0000

    quick comments

--- backend/src/main/java/com/hotel/reservations/entity/RoomEntity.java
+++ backend/src/main/java/com/hotel/reservations/entity/RoomEntity.java
@@ -1,16 +1,9 @@
 package com.hotel.reservations.entity;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
