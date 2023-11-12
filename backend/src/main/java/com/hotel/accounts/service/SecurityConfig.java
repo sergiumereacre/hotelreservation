8436b602ee68366f4554a4e5340c19de07802f2f
TheCaister <edisoncai2001@gmail.com>
2023-11-12 22:31:14 +0000

    config update for h2 UI

--- backend/src/main/java/com/hotel/accounts/service/SecurityConfig.java
+++ backend/src/main/java/com/hotel/accounts/service/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic().disable();
+                http.headers().frameOptions().sameOrigin();
     }
 
 }
