92a6b7c29f3d69c33e0816d3c3d376cbd4326e5b
sergiu <sergiumer@gmail.com>
2023-11-17 18:15:03 +0000

    Added id to card entity.
    Signed-off-by: sergiu <sergiumer@gmail.com>

--- backend/src/main/java/com/hotel/payments/entity/CardEntity.java
+++ backend/src/main/java/com/hotel/payments/entity/CardEntity.java
@@ -1,6 +1,9 @@
 package com.hotel.payments.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
@@ -9,6 +12,11 @@ import lombok.Data;
 @Entity
 @Data
 public class CardEntity {
+    // Card ID.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     // Card number is 16 digits.
     private int cardNumber;
 
