d9ce031be08eb3e569270f2aff760c8859a627e7
David Walsh <20276885@studentmail.ul.ie>
2023-11-17 18:22:17 +0000

    Updating ReviewController

--- backend/src/main/java/com/hotel/reviews/controller/ReviewController.java
+++ backend/src/main/java/com/hotel/reviews/controller/ReviewController.java
@@ -1,5 +1,60 @@
 package com.hotel.reviews.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stripe.model.Review;
+import com.stripe.service.ReviewService;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
-    
+
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Boolean> writeReview(@RequestParam int userId,
+                                               @RequestParam String resRef,
+                                               @RequestParam String reviewText,
+                                               @RequestParam int rating) {
+        boolean success = reviewService.writeReview(userId, resRef, reviewText, rating);
+        return ResponseEntity.ok(success);
+    }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<Boolean> editReview(@PathVariable int reviewId,
+                                              @RequestParam int userId,
+                                              @RequestParam String reviewText,
+                                              @RequestParam int rating) {
+        boolean success = reviewService.editReview(userId, reviewId, reviewText, rating);
+        return ResponseEntity.ok(success);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Boolean> deleteReview(@PathVariable int reviewId,
+                                                @RequestParam int userId) {
+        boolean success = reviewService.deleteReview(reviewId, userId);
+        return ResponseEntity.ok(success);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Review>> getReviewsByUser(@PathVariable int userId) {
+        List<Review> reviews = reviewService.getReviewList(userId);
+        return ResponseEntity.ok(reviews);
+    }
 }
